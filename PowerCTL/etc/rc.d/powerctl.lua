66fa02638c991fa7e0b25ecdd41d6ecf0527b708
tomato3017 <tomato3017@gmail.com>
2016-04-03 01:14:02 -0500

    ad

--- PowerCTL/etc/rc.d/powerctl.lua
+++ PowerCTL/etc/rc.d/powerctl.lua
@@ -12,51 +12,51 @@ local timers = {}
 
 
 local function loadconfig(filename)
-	if(fs.exists(filename)) then
-		local file = io.open(filename)
-		local config = appconf.parse(file:read("*a"))
-		file:close()
+    if(fs.exists(filename)) then
+        local file = io.open(filename)
+        local config = appconf.parse(file:read("*a"))
+        file:close()
 
-		return config
-	end
+        return config
+    end
 end
 
 local function msg(msg, isDebug)
-	if((isDebug and debug and msg)) then
-		print("POWERCTL-DEBUG: " .. msg)
-	elseif(not isDebug and msg) then
-		print("POWERCTL: " .. msg)
-	end
+    if((isDebug and debug and msg)) then
+        print("POWERCTL-DEBUG: " .. msg)
+    elseif(not isDebug and msg) then
+        print("POWERCTL: " .. msg)
+    end
 end
 
 local function isRunning()
-	return not not timers.powerpoll
+    return not not timers.powerpoll
 end
 
 local function powerpoll()
-	msg("Polling Power", true)
+    msg("Polling Power", true)
 end
 
 
 
 function start()
-	if(isRunning()) then
-		msg("POWERCTL is already running")
-		return
-	end
+    if(isRunning()) then
+        msg("POWERCTL is already running")
+        return
+    end
 
-	config = loadconfig("/etc/powerctl.cfg")
+    config = loadconfig("/etc/powerctl.cfg")
 
-	timers.powerpoll = event.timer(config.general.pollrate, powerpoll, math.huge)
-	
+    timers.powerpoll = event.timer(config.general.pollrate, powerpoll, math.huge)
+    
 end
 
 
 function stop()
-	if(not isRunning()) then
-		msg("POWERCTL is already stopped!")
-		return
-	end
-	event.cancel(timers.powerpoll)
-	timers.powerpoll = nil
+    if(not isRunning()) then
+        msg("POWERCTL is already stopped!")
+        return
+    end
+    event.cancel(timers.powerpoll)
+    timers.powerpoll = nil
 end
