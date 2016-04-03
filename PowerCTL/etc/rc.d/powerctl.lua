049eeec6ee454a22cd9e42e7d75e885b81013a1c
tomato3017 <tomato3017@gmail.com>
2016-04-03 00:40:54 -0500

    powerctl

--- PowerCTL/etc/rc.d/powerctl.lua
+++ PowerCTL/etc/rc.d/powerctl.lua
@@ -1,6 +1,25 @@
-local PowerCTL = {}
+--PowerCTL controller
+--By tomato3017
 
-function PowerCTL.loadconfig(filename)
+local fs = require("filesystem")
+local appconf = require("applicationconfparser")
+
+local function loadconfig(filename)
+	if(fs.exists(filename)) then
+		local file = io.open(filename)
+		local config = appconf(file:read("*a"))
+		file:close()
+
+		return config
+	end
+end
 
 
+local config
+
+
+function start()
+	config = loadconfig("/etc/powerctl.cfg")
+
+	print(config.pollrate)
 end
