05c9570ce065dde84fd8f4f9af9e32257e323d90
tomato3017 <tomato3017@gmail.com>
2016-04-03 01:04:28 -0500

    timers

--- PowerCTL/etc/rc.d/powerctl.lua
+++ PowerCTL/etc/rc.d/powerctl.lua
@@ -1,8 +1,11 @@
 --PowerCTL controller
 --By tomato3017
 
+local debug = true
+
 local fs = require("filesystem")
 local appconf = require("applicationconfparser")
+local event = require("event")
 
 local function loadconfig(filename)
 	if(fs.exists(filename)) then
@@ -14,12 +17,46 @@ local function loadconfig(filename)
 	end
 end
 
+local function msg(msg, isDebug)
+	if((isDebug and debug and msg) then
+		print("POWERCTL-DEBUG: " .. msg)
+	elseif(not isDebug and msg) then
+		print("POWERCTL: " .. msg)
+	end
+end
+
+local function isRunning()
+	return not not timers.powerpoll
+end
+
+local function powerpoll()
+	msg("Polling Power", true)
+end
+
+
 
 local config
+local timers = {}
 
 
 function start()
+	if(isRunning()) then
+		msg("POWERCTL is already running")
+		return
+	end
+
 	config = loadconfig("/etc/powerctl.cfg")
 
-	print(config.pollrate)
+	timers.powerpoll = event.timer(config.general.pollrate, powerpoll, math.huge)
+	
+end
+
+
+function stop()
+	if(not isRunning()) then
+		msg("POWERCTL is already stopped!")
+		return
+	end
+	event.cancel(timers.powerpoll)
+	timers.powerpoll = nil
 end
