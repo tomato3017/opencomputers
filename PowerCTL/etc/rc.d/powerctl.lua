ede528cfa20ec28f07795fcb8fb603a3b7d08185
tomato3017 <tomato3017@gmail.com>
2016-04-03 05:29:50 -0500

    Test

--- PowerCTL/etc/rc.d/powerctl.lua
+++ PowerCTL/etc/rc.d/powerctl.lua
@@ -6,10 +6,21 @@ local debug = true
 local fs = require("filesystem")
 local appconf = require("applicationconfparser")
 local event = require("event")
+local component = require("component")
 
 local config
 local timers = {}
+local MODES ={
+    ["NORMAL"] = 1,
+    ["BYPASS"] = 2,
+    ["OFFLINE"] = 3,
+    ["CUSTOM"] = 4
+}
 
+
+local current_mode = 0
+local lowpowerpercent = 20
+local highpowerpercent = 90
 
 local function loadconfig(filename)
     if(fs.exists(filename)) then
@@ -39,7 +50,35 @@ end
 
 local function modemHandler(name, _, _, port, _, msg)
     if(tonumber(port) == 10) then
-        print(msg)
+        local powerlevel, maxpowerlevel = string.match(msg,"PL=(%d+)|MPL=(%d+)")
+
+        if((current_mode == MODES.NORMAL) and ((powerlevel/maxpowerlevel) * 100) < lowpowerpercent) then
+            msg("Low power detected! Switching to bypass mode")
+            setMode(MODES.BYPASS)
+        end
+    end
+end
+
+local function setMode(mode, settings)
+    msg("SETTING MODE:" .. tostring(mode), true)
+
+    if(mode == MODES.NORMAL) then
+        print("NORMAL MODE")
+    elseif(mode == MODES.BYPASS)
+        print("BYPASS MODE")
+    elseif(mode == MODES.OFFLINE)
+        print("OFFLINE MODE")
+    end
+end
+
+
+function mode(msg)
+    if msg then
+        msg=string.upper(msg)
+    end
+
+    if(mode and MODES[msg]) then
+        setMode(mode[msg])
     end
 end
 
@@ -55,7 +94,8 @@ function start()
     event.listen("modem_message", modemHandler)
     component.modem.open(10)
 
-    --timers.powerpoll = event.timer(config.general.pollrate, powerpoll, math.huge)
+    setMode(MODES.NORMAL)
+    timers.powerpoll = event.timer(config.general.pollrate, powerpoll, math.huge)
     
 end
 
